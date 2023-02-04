11791078d2d4c3d6d614e30045776817d0c7df96
Jo'rabek <jorabek@olcha.uz>
2023-02-04 17:50:14 +0500

    select

--- category_create.php
+++ category_create.php
@@ -1,4 +1,10 @@
 <?php
+include_once './php/DB.php';
+
+$DBclass = new DB();
+$db = $DBclass->getDB();
+$genres = $db->query('select * from genres')->fetch_all(MYSQLI_ASSOC);
+
 ?>
 <!DOCTYPE html>
 <html class="loading dark-layout" lang="en" data-layout="dark-layout" data-textdirection="ltr">
@@ -8,16 +14,21 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0,minimal-ui">
-    <meta name="description" content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
-    <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
+    <meta name="description"
+          content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
+    <meta name="keywords"
+          content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
     <meta name="author" content="PIXINVENT">
     <title>Dashboard ecommerce - Vuexy - Bootstrap HTML admin template</title>
     <link rel="apple-touch-icon" href="./app-assets/images/ico/apple-icon-120.png">
     <link rel="shortcut icon" type="image/x-icon" href="./app-assets/images/ico/favicon.ico">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600"
+          rel="stylesheet">
 
     <!-- BEGIN: Vendor CSS-->
     <link rel="stylesheet" type="text/css" href="./app-assets/vendors/css/vendors.min.css">
+    <link rel="stylesheet" type="text/css" href="./app-assets/vendors/css/forms/select/select2.min.css">
+
     <!-- END: Vendor CSS-->
 
     <!-- BEGIN: Theme CSS-->
@@ -42,7 +53,8 @@
 
 <!-- BEGIN: Body-->
 
-<body class="vertical-layout vertical-menu-modern  navbar-floating footer-static  " data-open="click" data-menu="vertical-menu-modern" data-col="">
+<body class="vertical-layout vertical-menu-modern  navbar-floating footer-static  " data-open="click"
+      data-menu="vertical-menu-modern" data-col="">
 
 <!-- BEGIN: Header-->
 <?php include_once './app-assets/components/navbar.php'; ?>
@@ -79,8 +91,25 @@
                                                     <label class="col-form-label" for="first-name">Category Name</label>
                                                 </div>
                                                 <div class="col-sm-9">
-                                                    <input type="text" id="first-name" class="form-control" name="name" placeholder="Category Name" />
+                                                    <input type="text" id="first-name" class="form-control" name="name"
+                                                           placeholder="Category Name"/>
                                                 </div>
+
+                                        </div>
+                                        <div class="col-12">
+                                            <div class="mb-1 row">
+                                            <div class="col-sm-3">
+                                                <label class="col-form-label" for="select2-multiple">Multiple</label>
+                                            </div>
+
+                                            <div class="col-sm-9">
+                                                <select class="select2 form-select" id="select2-multiple" name="genre_ids"  >
+                                                    <?php foreach ($genres as $genre) { ?>
+                                                        <option value="<?= $genre['id']?>"><?= $genre['name']?></option>
+
+                                                    <?php }?>
+                                                </select>
+                                            </div>
                                             </div>
                                         </div>
 
@@ -117,7 +146,7 @@
 <!-- BEGIN Vendor JS-->
 
 <!-- BEGIN: Page Vendor JS-->
-
+<script src="./app-assets/vendors/js/forms/select/select2.full.min.js"></script>
 <!-- END: Page Vendor JS-->
 
 <!-- BEGIN: Theme JS-->
@@ -126,10 +155,12 @@
 <!-- END: Theme JS-->
 
 <!-- BEGIN: Page JS-->
+<script src="./app-assets/js/scripts/forms/form-select2.js"></script>
+
 <!-- END: Page JS-->
 
 <script>
-    $(window).on('load', function() {
+    $(window).on('load', function () {
         if (feather) {
             feather.replace({
                 width: 14,
--- movies.sql
+++ movies.sql
@@ -1,104 +1,126 @@
 create database movies;
-create table movie(
-                      id int auto_increment primary key,
-                      title nvarchar(255),
-                      category_id int,
-                      genre_id int,
-                      realise_date year,
-                      rate decimal,
-                      quality_id int,
-                      age int,
-                      run_time time,
-                      description longtext
+create table movie
+(
+    id           int auto_increment primary key,
+    title        nvarchar(255),
+    category_id  int,
+    genre_id     int,
+    realise_date year,
+    rate         decimal,
+    quality_id   int,
+    age          int,
+    run_time     time,
+    description  longtext
 );
 
-create table categories(
-                         id int primary key auto_increment,
-                         name varchar(30)
-);
-alter table movie
-    add foreign key (category_id) references categories(id);
-
-create table genre(
-                      id int primary key auto_increment,
-                      name varchar(30)
+create table categories
+(
+    id   int primary key auto_increment,
+    name varchar(30)
 );
 alter table movie
-    add foreign key (genre_id) references genres(id);
+    add foreign key (category_id) references categories (id);
 
-create table quality(
-                        id int primary key auto_increment,
-                        name varchar(30) /* yoki type db ketish keremi?*/
+create table genre
+(
+    id   int primary key auto_increment,
+    name varchar(30)
+);
+alter table movie
+    add foreign key (genre_id) references genres (id);
+
+create table quality
+(
+    id   int primary key auto_increment,
+    name varchar(30) /* yoki type db ketish keremi?*/
 );
 
 alter table movie
     add column country_id int;
 
-create table country(
-                        id int primary key auto_increment,
-                        name varchar(30)
+create table country
+(
+    id   int primary key auto_increment,
+    name varchar(30)
 );
 
 alter table movie
-    add foreign key (country_id) references country(id);
+    add foreign key (country_id) references country (id);
 
-create table plan(
-                     id int primary key auto_increment,
-                     type varchar(30),
-                     cost decimal
+create table plan
+(
+    id   int primary key auto_increment,
+    type varchar(30),
+    cost decimal
 );
 
-create table user(
-                     id int primary key auto_increment,
-                     name varchar(30),
-                     email varchar(30),
-                     password varchar(30)
+create table user
+(
+    id       int primary key auto_increment,
+    name     varchar(30),
+    email    varchar(30),
+    password varchar(30)
 );
 
-create table Serials(
-    id int auto_increment primary key ,
+create table Serials
+(
+    id       int auto_increment primary key,
     movie_id int,
-    status boolean,
-    title nvarchar(30)
+    status   boolean,
+    title    nvarchar(30)
 );
 
 alter table serials
-add foreign key (movie_id) references movie(id);
+    add foreign key (movie_id) references movie (id);
 
-create table Comments(
-    id int primary key auto_increment,
-    media_id int,
+create table Comments
+(
+    id         int primary key auto_increment,
+    media_id   int,
     media_type boolean,
-    user_id int,
-    reply_id int,
-    likes int,
-    dislike int,
-    text text,
+    user_id    int,
+    reply_id   int,
+    likes      int,
+    dislike    int,
+    text       text,
     created_at date,
-    foreign key (user_id) references user(id)
+    foreign key (user_id) references user (id)
 );
-insert into categories(name) VALUES ('comedy');
+insert into categories(name)
+VALUES ('comedy');
 
 alter table serials
-add column image nvarchar(30);
+    add column image nvarchar(30);
 
 
 alter table movie
     add column image nvarchar(30);
 
-create table serial_images(
-    id int primary key auto_increment,
-    image nvarchar(30),
+create table serial_images
+(
+    id        int primary key auto_increment,
+    image     nvarchar(30),
     serial_id int,
-    foreign key (serial_id) references serials(id)
+    foreign key (serial_id) references serials (id)
 );
 
-create table movie_images(
-                              id int primary key auto_increment,
-                              image nvarchar(30),
-                              movie_id int,
-                              foreign key (movie_id) references movie(id)
+create table movie_images
+(
+    id       int primary key auto_increment,
+    image    nvarchar(30),
+    movie_id int,
+    foreign key (movie_id) references movie (id)
 );
 
 alter table country
-rename country
+    rename country;
+alter table serials
+    drop foreign key serials_ibfk_1;
+
+alter table serials
+    drop column movie_id;
+
+alter table movie
+    add serial_id int null;
+
+
